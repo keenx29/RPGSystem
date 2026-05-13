a509182b81cd4302996d2e9057db7e9c7d2cd420
keenx29 <xkeen29@gmail.com>
2026-05-13 18:48:01 +0300

    Refactor character stats into reusable stat card partial views

---
+++ RPGSystem/Models/StatCardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RPGSystem.Models
+{
+    public class StatCardViewModel
+    {
+        public string StatName { get; set; } = "";
+        public int StatValue { get; set; }
+        public int Modifier { get; set; }
+    }
+}
